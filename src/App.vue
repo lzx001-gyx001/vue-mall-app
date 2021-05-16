b3ab1ea62e61dd46bbde8e5640bd86e4d688c343
unknown <595469645@qq.com>
2021-05-16 23:45:19 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="box center">
+      <span>刘高浔</span>
+      <span>刘高墨</span>
     </div>
-    <router-view/>
+    <button @click="handleClick" class="btn1">选名</button>
+    <button @click="handleCancel" class="btn2">清空</button>
+    <div class="name-box">{{this.name}}</div>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      name1: "刘高浔",
+      name2: "刘高墨"
+    };
+  },
+  methods: {
+    handleClick() {
+      let random = Math.random() * 10 + 1;
+      let num = Math.round(random);
+      console.log(num);
+      if (num > 5) {
+        this.name = this.name1;
+      } else {
+        this.name = this.name2;
+      }
+    },
+    handleCancel() {
+      this.name = "";
     }
   }
+};
+</script>
+
+<style lang="scss">
+#app {
+  background-color: #333;
+  width: 100vw;
+  height: 100vh;
+  color: #fff;
+  font-size: 50px;
+  & .box {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+.btn1,
+.btn2 {
+  width: 4rem;
+  height: 2.5rem;
+  font-size: 1rem;
+  color: #ff0f00;
+  background-color: #e3e4e5e6;
+}
+.name-box {
+  margin: 0 auto;
+  padding: 0.5rem;
+  border-radius: 0.3rem;
+  width: 5rem;
+  text-align: center;
+  margin-top: 50px;
+  color: #ff0;
+  background-color: #777;
 }
 </style>
